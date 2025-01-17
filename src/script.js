73a561e615c00d80e3e08a5c3de49814682bba55
sandramerc <144231118+sandramercadal@users.noreply.github.com>
2025-01-17 14:18:11 +0000

    Building the current date and time.

--- src/script.js
+++ src/script.js
@@ -1,41 +1,37 @@
-let weather = {
-  paris: {
-    temp: 19.7,
-    humidity: 80,
-  },
-  tokyo: {
-    temp: 17.3,
-    humidity: 50,
-  },
-  lisbon: {
-    temp: 30.2,
-    humidity: 20,
-  },
-  "san francisco": {
-    temp: 20.9,
-    humidity: 100,
-  },
-  oslo: {
-    temp: -5,
-    humidity: 20,
-  },
-};
-
-let city = prompt("Enter a city");
-city = city.toLowerCase();
-if (weather[city] !== undefined) {
-  let temperature = weather[city].temp;
-  let humidity = weather[city].humidity;
-  let celsiusTemperature = Math.round(temperature);
-  let fahrenheitTemperature = Math.round((temperature * 9) / 5 + 32);
-
-  alert(
-    `It is currently ${celsiusTemperature}°C (${fahrenheitTemperature} °F) in ${city} 
-  with a humidity of ${humidity}%`
-  );
-} else {
-  alert(
-    `Sorry we don't have the weather for this city, 
-    try going to https://www.google.com/search?q=weather+${city}`
-  );
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = searchInputElement.value;
 }
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+
+let currentDate = document.querySelector("#current-date");
+Let currentTime = new Date ();
+let minutes = currentTime.getMinutes();
+let hours = currentTime.getHours();
+let day = currentTime.getDay();
+
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let formattedDay = days[day];
+
+currentDate.innerHTML = `${formattedDay} ${hours}:${minutes}`;
